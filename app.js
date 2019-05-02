f7dd488cd064a808022539e167879590de119d87
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-01 22:28:29 -0400

    refactored animal variables into animal object

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
 // assigning html div elements to variables.
-let cow = document.querySelector('#cow');
-let chicken = document.querySelector('#chicken');
-let pig = document.querySelector('#pig');
-let horse = document.querySelector('#horse');
-let sheep = document.querySelector('#sheep');
-let duck = document.querySelector('#duck');
-let dog = document.querySelector('#dog');
-let bird = document.querySelector('#bird');
-let cat = document.querySelector('#cat');
+let animals = {
+    cow: document.querySelector('#cow'),
+    chicken: document.querySelector('#chicken'),
+    pig: document.querySelector('#pig'),
+    horse: document.querySelector('#horse'),
+    sheep: document.querySelector('#sheep'),
+    duck: document.querySelector('#duck'),
+    dog: document.querySelector('#dog'),
+    bird: document.querySelector('#bird'),
+    cat: document.querySelector('#cat')
+}
+
 
 // assigning audio elements to variables.
 let cowsound = document.querySelector('#cowsound');
@@ -22,39 +25,39 @@ let birdsound = document.querySelector('#birdsound');
 let catsound = document.querySelector('#catsound');
 
 
-cow.addEventListener('click', function() {
+animals.cow.addEventListener('click', function() {
     cowsound.play();
 });
 
-chicken.addEventListener('click', function() {
+animals.chicken.addEventListener('click', function() {
     chickensound.play();
 });
 
-pig.addEventListener('click', function() {
+animals.pig.addEventListener('click', function() {
     pigsound.play();
 });
 
-horse.addEventListener('click', function() {
+animals.horse.addEventListener('click', function() {
     horsesound.play();
 });
 
-sheep.addEventListener('click', function() {
+animals.sheep.addEventListener('click', function() {
     sheepsound.play();
 });
 
-duck.addEventListener('click', function() {
+animals.duck.addEventListener('click', function() {
     ducksound.play();
 });
 
-dog.addEventListener('click', function() {
+animals.dog.addEventListener('click', function() {
     dogsound.play();
 });
 
-bird.addEventListener('click', function() {
+animals.bird.addEventListener('click', function() {
     birdsound.play();
 });
 
-cat.addEventListener('click', function() {
+animals.cat.addEventListener('click', function() {
     catsound.play();
 });
 
